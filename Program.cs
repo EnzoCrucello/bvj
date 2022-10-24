0804729b0af3afc518a670f5e8f402be8d02dfd5
Enzo <114952661+EnzoCrucello@users.noreply.github.com>
2022-10-24 18:04:10 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace esse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Funcionario #1 Dados:");
+        }
+    }
+}
